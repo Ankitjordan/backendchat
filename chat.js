9db3806451fc2c883f9ef52d823f88cf40fd2867
Ankitjordan <ankityar13@gmail.com>
2025-06-20 18:16:24 +0530

    debug

--- chat.js
+++ chat.js
@@ -11,10 +11,11 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://frontend-logic-chatapp.vercel.app/"],
+    origin: ["https://frontend-logic-chatapp.vercel.app"],
     methods: ["GET", "POST"], // yeh likhna safe & recommended hai
   },
 });
+app.use(cors());
 
 io.on("connection", (socket) => {
   console.log("a user connected");
@@ -33,7 +34,6 @@ app.get("/", (req, res) => {
   res.send("Hello World from backend!");
 });
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
